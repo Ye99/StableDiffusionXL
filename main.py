c7ceed941717bfa4cfaafe9473516c262adf7e34
Ye Zhang <mr.yezhang@gmail.com>
2023-11-04 18:15:10 -0700

    Format file.

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-from diffusers import DiffusionPipeline
 import torch
+from diffusers import DiffusionPipeline
 
-pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
+pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0",
+                                         torch_dtype=torch.float16,
+                                         use_safetensors=True, variant="fp16")
+# If your GPU has > 8GB vRAM
 # pipe.to("cuda")
+# If your GPU has < 8GB vRAM
 pipe.enable_model_cpu_offload()
 # pipe.enable_xformers_memory_efficient_attention()
 
 # if using torch < 2.0
 # pipe.enable_xformers_memory_efficient_attention()
-
 
 prompt = (input("describe the picture you want: "))
 
